62dca4517080c9a4b5b9e32117a8b65daca61e61
Nicolas Junkes <nicolas.junkes00@gmail.com>
2025-05-26 14:18:55 -0300

    fix: rename prop name

--- front/src/assets/styles/themes/default.js
+++ front/src/assets/styles/themes/default.js
@@ -3,7 +3,7 @@ export default {
     background: '#F6f5f5',
     primary: {
       lighter: '#E0E3FF',
-      ligth: '#6674F4',
+      light: '#6674F4',
       main: '#5061fc',
       dark: '#3346F0',
     },
