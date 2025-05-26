4369f4546956c26529f68c1be3f5fd65a436a013
Nicolas Junkes <nicolas.junkes00@gmail.com>
2025-05-26 00:43:05 -0300

    feat: create default theme and input search contact

---
+++ front/src/assets/styles/themes/default.js
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    background: '#F6f5f5',
+    primary: {
+      lighter: '#E0E3FF',
+      ligth: '#6674F4',
+      main: '#5061fc',
+      dark: '#3346F0',
+    },
+  },
+};
