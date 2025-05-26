cfa69d2e04d6c51b7dac664495f81a687a1d98fb
Nicolas Junkes <nicolas.junkes00@gmail.com>
2025-05-26 14:21:22 -0300

    feat: create form components

---
+++ front/src/components/Select.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export default styled.select`
+  width: 100%;
+  border: none;
+  background: #fff;
+  border: 2px solid #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  height: 3.25rem; // 52px
+  border-radius: 0.25rem; // 4px
+  outline: none;
+  padding: 0 1rem; // 16px
+  font-size: 1rem; // 16px
+  transition: border-color 0.2s ease-in;
+  /* appearance: none; // Remove default arrow */
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary.main};
+  }
+`;
