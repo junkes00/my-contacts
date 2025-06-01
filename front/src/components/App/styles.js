019be556ff116938989148b8904048da56e5a004
Nicolas Junkes <nicolas.junkes00@gmail.com>
2025-06-01 20:58:06 -0300

    style: add padding to better mobile ui

--- front/src/components/App/styles.js
+++ front/src/components/App/styles.js
@@ -3,5 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   max-width: 31.25rem; // 500px
-  margin: 0 auto
+  margin: 0 auto;
+  padding: 0 1rem;
 `;
