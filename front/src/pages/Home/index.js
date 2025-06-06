ddd3b2813572c6e75125fddbef17dc679cefe6d0
Nicolas Junkes <nicolas.junkes00@gmail.com>
2025-06-06 17:15:39 -0300

    feat: create contacts service

--- front/src/pages/Home/index.js
+++ front/src/pages/Home/index.js
@@ -13,7 +13,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
 import Loader from '../../components/Loader';
-import delay from '../../utils/delay';
+import ContactsService from '../../services/ContactsService';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
@@ -29,12 +29,9 @@ export default function Home() {
       try {
         setIsLoading(true);
 
-        const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
+        const contactsList = await ContactsService.listContacts(orderBy);
 
-        await delay(500);
-
-        const json = await response.json();
-        setContacts(json);
+        setContacts(contactsList);
       } catch (error) {
         console.error('Error: ', error);
       } finally {
