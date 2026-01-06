49e391d9319f0deade1a2cec89d31b27ac3d52a2
Nicolas Junkes <nicolas.junkes00@gmail.com>
2026-01-06 11:15:45 -0300

    feat: add new endpoint to create contacts

--- front/src/pages/NewContact/index.js
+++ front/src/pages/NewContact/index.js
@@ -2,11 +2,28 @@ import PageHeader from '../../components/PageHeader';
 
 import ContactForm from '../../components/ContactForm';
 
+import ContactsService from '../../services/ContactsService';
+
 export default function NewContact() {
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        ...formData,
+        category_id: formData.categoryId,
+      };
+
+      const response = await ContactsService.createContact(contact);
+
+      console.log(response);
+    } catch {
+      alert('Ocorreu um erro ao cadastrar o contato');
+    }
+  }
+
   return (
     <>
       <PageHeader title="Novo contato" />
-      <ContactForm buttonLabel="Cadastrar" />
+      <ContactForm buttonLabel="Cadastrar" onSubmit={handleSubmit} />
     </>
   );
 }
